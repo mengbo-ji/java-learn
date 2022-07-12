25f924b2b01b82109788f8177434a9a1ffb040c8
mengbo-ji <jmb33595@163.com>
2022-07-12 23:20:20 +0800

    feat: string类型,二进制,八进制,十进制,十六进制学习

---
+++ code/day02/StringTest.java
@@ -0,0 +1,13 @@
+/**
+ * String属于引用类型
+ * 声明String类型的变量用一对 ""包裹
+*/
+class StringTest {
+  public static void main(String[] args) {
+    String s1 = "hello world";
+    String s2 = "";
+
+    // char c1 = ''; // 编译不通过
+    System.out.println(s1);;
+  }
+}
