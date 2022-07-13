f86865cf55c2484ae30179b3310ac9fa448fb190
mengbo-ji <jmb33595@163.com>
2022-07-13 17:52:14 +0800

    feat: 类型学习

---
+++ code/day02/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+  String name;
+
+  public Employee(String name) {
+    this.name = name;
+  }
+
+  public void printName() {
+    System.out.println(this.name);
+  }
+}
---
+++ code/day02/EmployeeTest.java
@@ -0,0 +1,7 @@
+public class EmployeeTest {
+  public static void main(String[] args) {
+    Employee myEmployee = new Employee("张三");
+
+    myEmployee.printName();
+  }
+}
