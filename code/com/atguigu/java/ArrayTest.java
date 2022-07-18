54f554f2f24e7542018f89a71070bcf5836fb1ac
mengbo-ji <jmb33595@163.com>
2022-07-18 23:48:50 +0800

    idea 学习

---
+++ code/com/atguigu/java/ArrayTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.java;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[]{1, 2, 3};
+        String[] arr1 = new String[5];
+        arr1 = new String[]{"name"};
+        System.out.println(arr1[0]);
+    }
+}
